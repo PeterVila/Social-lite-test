1aad00a245a22f8c8ed192fb86421add5a2a3017
Peter Vila <peterdvila@gmail.com>
2021-10-30 22:56:32 -0700

    Added post and event styles

---
+++ script.js
@@ -0,0 +1,6 @@
+const downChevron = document.querySelector('.bottom-arrow');
+const extraInfo = document.querySelector('.extra-info');
+downChevron.addEventListener('click', () => {
+    console.log('yoyoyo')
+    extraInfo.className = 'extra-info pop'
+})
